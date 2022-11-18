0257a397622468b3fdc7f1561bc240f508dfdc37
ragnaroksj <ragnaroksj@gmail.com>
2022-11-17 16:02:57 -0800

    add vitest

---
+++ pages/index.test.tsx
@@ -0,0 +1,22 @@
+import { expect, it, describe } from 'vitest'
+import { render, screen, within } from '@testing-library/react'
+import Home, { getStaticProps } from './index'
+
+describe('home', () => {
+  it('renders homepage correctly', () => {
+    const allPostsData = [
+      {id: 'a', date: '2022-01-02', title: 'a'},
+      {id: 'b', date: '2022-01-02', title: 'b'}
+    ];
+  
+    const result = render(<Home allPostsData={allPostsData}/>)
+    expect(result).toMatchSnapshot()
+  });
+
+  it('getStaticProps', async () => {
+    const localeSetting = {locale: 'en'};
+    const result = await getStaticProps(localeSetting);
+    
+    expect(result).toMatchSnapshot();
+  })
+})
